e5b24041cb31034151452574651dfe71d8c120e2
manggala25 <manggalaaria2505@gmail>
2024-07-09 00:04:08 +0700

    rename folder frontend-dpmptsp to dpmptsp-master

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -9,22 +9,22 @@
     {{-- <link rel="shortcut icon" href="images/logo-only.jpg"> --}}
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="{{ asset('template/frontend-dpmptsp/html/css/bootstrap.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('template/dpmptsp-master/html/css/bootstrap.min.css') }}" type="text/css">
 
     <!--Material Icon -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('template/frontend-dpmptsp/html/css/materialdesignicons.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/dpmptsp-master/html/css/materialdesignicons.min.css') }}" />
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('template/frontend-dpmptsp/html/css/fontawesome.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/dpmptsp-master/html/css/fontawesome.css') }}" />
 
     <!-- selectize css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('template/frontend-dpmptsp/html/css/selectize.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/dpmptsp-master/html/css/selectize.css') }}" />
 
     <!--Slider-->
-    <link rel="stylesheet" href="{{ asset('template/frontend-dpmptsp/html/css/owl.carousel.css') }}" />
-    <link rel="stylesheet" href="{{ asset('template/frontend-dpmptsp/html/css/owl.theme.css') }}" />
-    <link rel="stylesheet" href="{{ asset('template/frontend-dpmptsp/html/css/owl.transitions.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template/dpmptsp-master/html/css/owl.carousel.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template/dpmptsp-master/html/css/owl.theme.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template/dpmptsp-master/html/css/owl.transitions.css') }}" />
 
     <!-- Custom  Css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('template/frontend-dpmptsp/html/css/style.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/dpmptsp-master/html/css/style.min.css') }}" />
 
 </head>
--- resources/views/components/script.blade.php
+++ resources/views/components/script.blade.php
@@ -1,15 +1,15 @@
 <!-- javascript -->
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/jquery.easing.min.js') }}"></script>
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/plugins.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/plugins.js') }}"></script>
 
     <!-- selectize js -->
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/selectize.min.js') }}"></script>
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/selectize.min.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/jquery.nice-select.min.js') }}"></script>
 
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/counter.int.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/counter.int.js') }}"></script>
 
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/app.js') }}"></script>
-    <script src="{{ asset('template/frontend-dpmptsp/html/js/home.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/app.js') }}"></script>
+    <script src="{{ asset('template/dpmptsp-master/html/js/home.js') }}"></script>
